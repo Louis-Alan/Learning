ce76d56be88967e46b2bc2502ab179517e2945c0
Alan Louis <alouis811@gmail.com>
2023-04-04 17:24:34 +0530

    sliding window

--- sliding window/firstNegInK.py
+++ sliding window/firstNegInK.py
@@ -1,4 +1,18 @@
 def FirstNeg(arr, k):
+    i = 0
+    j = 0
+    neg = []
+    ans = []
+    while(j < len(arr)):
+        if(arr[j] < 0):
+            neg.append(arr[j])
+        if(j-i+1 == k):
+            ans.append(neg[0])
+            if(arr[i] == neg[0]):
+                neg.pop(0)
+            i += 1
+        j += 1
+    return ans
 
 arr = [12, -1, -7, 8, 9, -15, 30, 18]
 k = 3
